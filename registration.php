37251fe3f19e56f4939a983cdd3cf2d39821decb
Tem <tehmina3@rambler.ru>
2017-03-16 13:13:58 +0400

    db

--- registration.php
+++ registration.php
@@ -1,35 +1,45 @@
 <?php
-try{
-    $db = new PDO("mysql:host=localhost;dbname=chat", "root", "" );
-}catch (PDOException $error){
+try {
+    $db = new PDO("mysql:host=localhost;dbname=chat", "root", "");
+} catch (PDOException $error) {
     file_put_contents('error_logs', $error->getMessage());
 }
 
-$count=$db->exec("INSERT INTO users(username, email) VALUES ($name, $email)");
+
+//$count = $db->exec("INSERT INTO users(username, email) VALUES ($name, $email)");
+
+if (isset($_POST['register'])) {
+    try {
+
+        $select = $db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+        $select->bindParam(":username", $_POST['username'], PDO::PARAM_STR);
+        $select->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
+        $select->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
 
 
-try{
-    $select = $db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-    $select->bindParam(":username", $_POST['username'], PDO::PARAM_STR);
-    $select->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
-    $select->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
-    $select->execute();
-}catch (PDOException $e){
-    echo $e->getMessage();
+        if ($select->execute()) {
+            echo "Registered";
+        } else {
+            echo "Query could not execute!";
+        }
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+/*if(isset($_POST['login'])) {
+    try {
+        $select = $db->prepare("SELECT * FROM users WHERE email= :email AND password= :password");
+        $select->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
+        $select->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
+        $select->execute();
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    $userdata = $select->fetchAll();
 }
 
-
-$select = $db->prepare("SELECT * FROM users WHERE email= :email AND pasword= :pasword");
-$select->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
-$select->bindParam(":pasword", $pasword, PDO::PARAM_STR);
-$select->execute();
-
-
-$userdata = $select->fetchAll();
-
-
-
-
-
-
 echo $count;
+*/
+
